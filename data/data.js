ef09e6462f5e9bdc95283d2e981ab8681ca4cb48
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-12-18 22:58:38 +0530

    Update data.js

--- data/data.js
+++ data/data.js
@@ -11,16 +11,4 @@ export const events = [
     eventDescription:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
-  {
-    eventId: "4656547567",
-    eventOrganizer: "Some Name",
-    eventImage: "/sampleEventPhoto.jpg",
-    eventName: "Coldplay Music of the Spheres",
-    eventPlace: "SRM Institute of Science and Technology",
-    eventAddress: "Srm Nagar, Kattankulathur, Chennai, Tamil Nadu 603203",
-    eventDate: " 15 Nov 2024",
-    eventTime: "4pm - 12pm",
-    eventDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  },
 ];
