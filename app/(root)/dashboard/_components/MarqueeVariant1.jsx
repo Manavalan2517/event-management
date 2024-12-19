bfab3ac111424d7e7b9efc515ce73ccbd9816a6d
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-12-19 18:35:38 +0530

    Update MarqueeVariant1.jsx

--- app/(root)/dashboard/_components/MarqueeVariant1.jsx
+++ app/(root)/dashboard/_components/MarqueeVariant1.jsx
@@ -43,8 +43,8 @@ const MarqueeDemo = () => {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
-        {testimonials.map((review) => (
-          <ReviewCard key={review.username} {...review} />
+        {testimonials.map((review, i) => (
+          <ReviewCard key={i} {...review} />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
